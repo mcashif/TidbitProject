b7e4bc41f107079292b7530720f9cc0f21d5e087
rrsiii <Rrsiii@gmail.com>
2016-08-19 16:56:25 +0500

    Cleanup

--- TidbitProject/settings.py
+++ TidbitProject/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tidbit',
-    'rest_framework',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -116,13 +115,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
